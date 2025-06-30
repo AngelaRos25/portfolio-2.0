ff7ad91187c384034e4f683dcd5ac55ffb21d1c9
Giangy25 <angirosace99@gmail.com>
2025-06-30 19:50:24 +0200

    aggiugo icon curriculum

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
+import { IoDocumentText } from "react-icons/io5";
+
 
 import './Footer.css';
 
@@ -11,6 +13,7 @@ function Footer(){
         <a href="https://www.instagram.com/angiirosi/" target="_blank"><FaInstagram /></a>
         <a href="https://www.linkedin.com/in/angela-rosace-744925291/"><FaLinkedin /></a>
         <a href="https://github.com/Giangy25"><FaGithub /></a>
+        <a><IoDocumentText /></a>
       </div>
       <div className="copyright">
         © 2025 Angela Rosace
