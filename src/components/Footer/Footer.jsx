f29a68bccdf31e64a5909c408edcea7773cfc883
Giangy25 <angirosace99@gmail.com>
2025-07-13 19:35:41 +0200

    aggiungo cv

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import { IoDocumentText } from "react-icons/io5";
-
+import pdf from '../../assets/document/CV-Angela-Rosace.pdf';
 
 import './Footer.css';
 
@@ -11,9 +11,9 @@ function Footer() {
     <footer>
       <div className="icon">
         <a href="https://www.instagram.com/angiirosi/" target="_blank"><FaInstagram /></a>
-        <a href="https://www.linkedin.com/in/angela-rosace-744925291/"><FaLinkedin /></a>
-        <a href="https://github.com/Giangy25"><FaGithub /></a>
-        <a><IoDocumentText /></a>
+        <a href="https://www.linkedin.com/in/angela-rosace-744925291/" target="_blank"><FaLinkedin /></a>
+        <a href="https://github.com/Giangy25" target="_blank"><FaGithub /></a>
+        <a href={pdf} target="_blank"><IoDocumentText /></a>
       </div>
       <div className="copyright">
         © 2025 Angela Rosace
