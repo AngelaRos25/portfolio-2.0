062e576e98b2ac1d36b4afd11402d13a4979bc25
Giangy25 <angirosace99@gmail.com>
2025-08-15 17:48:06 +0200

    sistemo

--- src/data/dataWork.jsx
+++ src/data/dataWork.jsx
@@ -6,21 +6,10 @@ export const DataWorkExperience = [
     description: "Mi occupo di smistare e consegnare la posta, raccomandate e pacchi, guidando i mezzi assegnati.",
   },
   {
-    time: "02/2022 - 06/2023",
-    title: "Addetta alla vendita, reparto ortofrutta",
-    company: "presso Cadoro",
-    description: "Mi occupavo di pulire e sistemare frutta e verdura , allestivo la mattina i banchi dell' ortofrutta e li mantenevo in ordine per tutto il giorno. Cercavo di aiutare i clienti consigliandogli come cucinare o conservare l'alimento. Dovevo saper applicare e conoscere le procedure haccp."
-  },
-  {
-    time: "10/2020 - 02/2022",
+    time: "03/2019 - 02/2022",
     title: "Addetta allo smistamento",
     company: "presso Poste Italiane",
     description: "Smistavo e tracciavo i pacchi nei turni notturni, con velocità e precisione per poter farli partire entro la fine del turno. Muovevo bancali grazie all'uso del transpallet. Utilizzavo macchinari per smistare la posta per gli uffici."
   },
-  {
-    time: "03/2019 - 09/2020",
-    title: "Addetta alla Preparazione, cottura e vendita",
-    company: "presso Mc Donald's",
-    description: "Controllavo che le materie prime fossero idonee alla vendita. Preparazione e cottura degli alimenti. Cura dell'igiene dei luoghi e del materiale operativo secondo le procedure haccp. Formavo i nuovi dipendenti sulle procedure lavorative. Mi occupavo della cassa, e delle richieste dei clienti."
-  }
+
 ]
